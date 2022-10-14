1ab6d1ace5774fb0eea4738d649c718c4fd27069
Josue Chavez <josue.chavezlomeli@gmail.com>
2022-10-13 21:30:24 -0500

    Se corrige la parte de Chris en opiniones_vet

--- src/main/java/com/generation/DrLomito/service/opiniones_vetService.java
+++ src/main/java/com/generation/DrLomito/service/opiniones_vetService.java
@@ -7,49 +7,55 @@ import com.generation.DrLomito.model.Opiniones_vet;
 
 @Service
 public class opiniones_vetService {
-	private final List<com.generation.DrLomito.service.opiniones_vetRepository> opiniones_vetRepository;
+	private final opiniones_vetRepository opiniones_vetRepository;
 	
 	@Autowired
-	public opiniones_vetService(List<com.generation.DrLomito.service.opiniones_vetRepository> opiniones_vetRepository) {
+	public opiniones_vetService(com.generation.DrLomito.service.opiniones_vetRepository opiniones_vetRepository) {
 		super();
 		this.opiniones_vetRepository = opiniones_vetRepository;
-	}//constructor opiniones_vetRepository
+	}
 
-	public List <opiniones_vetRepository> getOpiniones_vetRepository() {
-		return opiniones_vetRepository;
+	public List<Opiniones_vet> getOpinionesVet() {
+		return opiniones_vetRepository.findAll();
 	}//getOpiniones
 	
-	public Opiniones_vet getOpiniones_vet(Long id) {
+	public Opiniones_vet getOpinion_vet(Long id) {		
 		return opiniones_vetRepository.findById(id).orElseThrow(()->new 
-				IllegalArgumentException("Opinión" + id + "no es válida."));
-		
-		public Opiniones_vet deleteOpiniones_vet(Long id) {
-			Opiniones_vet tmpUs = null;
-			if(((CrudRepository<Opiniones_vet, Long>) opiniones_vetRepository).existsById(id)) {
-				tmpUs = ((CrudRepository<Opiniones_vet, Long>) opiniones_vetRepository).findById(id).get();
-				((CrudRepository<Opiniones_vet, Long>) opiniones_vetRepository).deleteById(id);
-			}//ifExist
-				return tmpUs;
-		}//deleteUsuario
-		
-		public Opiniones_vet addOpinion(Opiniones_vet Opiniones_vet) {
-			return opiniones_vetRepository.save(Opiniones_vet);
-		}//addUsuarios
-		
-		public Opiniones_vet updateOpiniones(Long id, String opiniones_nombre_autor, String opiniones_calificacion, String usuarios_usuario_id) {
-			Opiniones_vet tmpUs = null;
-			if(opiniones_vetRepository.existsById(id)) {
-				tmpUs = opiniones_vetRepository.findById(id).get();
-				if(opiniones_nombre_autor!=null) tmpUs.setOpiniones_nombre_autor(opiniones_nombre_autor);
-				if(opiniones_calificacion!=null) tmpUs.setOpiniones_calificacion(opiniones_calificacion);
-				if(usuarios_usuario_id!=null) tmpUs.setUsuarios_usuario_id(usuarios_usuario_id);
-				
-			}
-	
-		
-		}
-		
+				IllegalArgumentException("Opinión" + id + " no existe."));
+	}//getOpinionIndividual
 	
 	
+	  public Opiniones_vet deleteOpiniones_vet(Long id) { 
+		  Opiniones_vet tmpOpinion = null; 
+		  if(opiniones_vetRepository.existsById(id)) { 
+			  tmpOpinion = opiniones_vetRepository.findById(id).get();
+			  opiniones_vetRepository.deleteById(id); 
+			  }//ifExist return tmpOpinion;
+		  return tmpOpinion;
+	  }//deleteOpinion
+	  
+	  
+	  public Opiniones_vet addOpinion(Opiniones_vet Opiniones_vet) {
+		  return  opiniones_vetRepository.save(Opiniones_vet);
+	  }//addOpinion
+	  
+	  public Opiniones_vet updateOpiniones(Long id, String opiniones_nombre_autor,
+	  Integer opiniones_calificacion,String opiniones_comentario, Long usuarios_usuario_id) { 
+		  Opiniones_vet tmpOpinion = null;
+		  if(opiniones_vetRepository.existsById(id)) {
+			  tmpOpinion =  opiniones_vetRepository.findById(id).get(); 
+		  if(opiniones_nombre_autor!=null)tmpOpinion.setOpiniones_nombre_autor(opiniones_nombre_autor);
+	      if(opiniones_calificacion!=null) tmpOpinion.setOpiniones_calificacion(opiniones_calificacion.intValue());
+	      if(opiniones_comentario!=null) tmpOpinion.setOpiniones_comentario(opiniones_comentario);
+	      if(usuarios_usuario_id!=null) tmpOpinion.setUsuarios_usuario_id(usuarios_usuario_id.longValue());
+	      opiniones_vetRepository.save(tmpOpinion);
+	  }else{
+			System.out.println("Update - La opinion con el id " + id + " no existe");
+		}//if exists
+	  return tmpOpinion;	  
+	  }//update Opinion
+	  
+	  
+	 
 
 }//class opiniones_vetService
